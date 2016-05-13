bb899e971e3102f93d76e2fd1b9fbe444dd0ae7a
Chris Tran <christopher.huytran@gmail.com>
2016-05-13 13:43:24 -0500

    simple robot to respond to greetings

---
+++ robot.js
@@ -0,0 +1,18 @@
+var Botkit = require('botkit');
+require('dotenv').config();
+
+var controller = Botkit.slackbot({
+  debug: false
+});
+
+// connect the bot to a stream of messages
+controller.spawn({
+  token: process.env.API_TOKEN,
+}).startRTM()
+
+// give the bot something to listen for.
+controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
+
+  bot.reply(message,'Hello yourself.');
+
+});
